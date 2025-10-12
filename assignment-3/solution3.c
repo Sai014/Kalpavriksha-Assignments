0764c02b26dd4243691a3e49721a2ccc3e0077c7
Sai Sandeep R <sandeep.5112004@gmail.com>
2025-10-12 11:19:24 +0530

    Updated Student performance analyzer after mentor feedback: Changed structure name, Added macros for grade check, Created a function for marks check.

--- assignment-3/solution3.c
+++ assignment-3/solution3.c
@@ -3,6 +3,10 @@
 
 #define MAX_STUDENTS 100
 #define MAX_MARKS 100
+#define Grade_A 85
+#define Grade_B 70
+#define Grade_C 50
+#define Grade_D 35
 
 //Structure for student.
 typedef struct
@@ -12,14 +16,15 @@ typedef struct
     int Marks1;
     int Marks2;
     int Marks3;
-}students;
+}Student;
 
-void fetchDetails(const int numberOfStudents, students Student[]);
-void displayDetails(const int numberOfStudents, students Student[]);
+void fetchDetails(const int numberOfStudents, Student student[]);
+void displayDetails(const int numberOfStudents, Student student[]);
 void displayRollNumbers(const int numberOfStudents,int initial);
 char gradeCalculator(float Average);
 int performanceCalculator(char Grade);
 void printPerformance(float Average, int Performance);
+bool checkMarks(int Marks1,int Marks2,int Marks3);
 
 
 
@@ -33,10 +38,10 @@ int main()
         printf("Enter a valid size! (1-100)");
         return 1;
     }
-    students Student[numberOfStudents];
+    Student student[numberOfStudents];
 
-    fetchDetails(numberOfStudents,Student);
-    displayDetails(numberOfStudents,Student);
+    fetchDetails(numberOfStudents,student);
+    displayDetails(numberOfStudents,student);
     printf("List of Roll numbers(via recurssion): ");
     displayRollNumbers(numberOfStudents,1);
     printf("\n");
@@ -47,37 +52,33 @@ int main()
 
 
 //Function to fetch all the details of students.
-void fetchDetails(const int numberOfStudents, students Student[])
+void fetchDetails(const int numberOfStudents, Student student[])
 {
     printf("Enter Details: \n");
     for(int i = 0; i < numberOfStudents; i++) {
         do {
-            scanf("%d %s %d %d %d", &Student[i].RollNo, Student[i].Name,&Student[i].Marks1, &Student[i].Marks2, &Student[i].Marks3);
-            if(Student[i].Marks1 < 0 || Student[i].Marks1 > MAX_MARKS ||
-               Student[i].Marks2 < 0 || Student[i].Marks2 > MAX_MARKS ||
-               Student[i].Marks3 < 0 || Student[i].Marks3 > MAX_MARKS) 
+            scanf("%d %s %d %d %d", &student[i].RollNo, student[i].Name,&student[i].Marks1, &student[i].Marks2, &student[i].Marks3);
+            if(checkMarks(student[i].Marks1,student[i].Marks2,student[i].Marks3)) 
             {
                 printf("Invalid marks! Enter again (0-100):\n");
             }
-        } while(Student[i].Marks1 < 0 || Student[i].Marks1 > MAX_MARKS ||
-                Student[i].Marks2 < 0 || Student[i].Marks2 > MAX_MARKS ||
-                Student[i].Marks3 < 0 || Student[i].Marks3 > MAX_MARKS);
+        } while(checkMarks(student[i].Marks1,student[i].Marks2,student[i].Marks3));
     }
     printf("\n");
 }
 
 //Function to display all the details of students.
-void displayDetails(const int numberOfStudents, students Student[])
+void displayDetails(const int numberOfStudents, Student student[])
 {
     for(int i=0;i<numberOfStudents;i++)
     {
-        int Total=Student[i].Marks1+Student[i].Marks2+Student[i].Marks3;
+        int Total=student[i].Marks1+student[i].Marks2+student[i].Marks3;
         float Average=Total/3.0;
         char Grade=gradeCalculator(Average);
         int Performance=performanceCalculator(Grade);
 
-        printf("Roll No: %d\n", Student[i].RollNo);
-        printf("Name: %s\n", Student[i].Name);
+        printf("Roll No: %d\n", student[i].RollNo);
+        printf("Name: %s\n", student[i].Name);
         printf("Total: %d\n", Total);
         printf("Average: %.2f\n", Average);
         printf("Grade: %c\n", Grade);
@@ -99,10 +100,10 @@ void displayRollNumbers(const int numberOfStudents,int initial)
 //Function to calculate grade based on average.
 char gradeCalculator(float Average)
 {
-    if(Average>=85)return 'A';
-    else if(Average>=70)return 'B';
-    else if(Average>=50)return 'C';
-    else if(Average>=35)return 'D';
+    if(Average>=Grade_A)return 'A';
+    else if(Average>=Grade_B)return 'B';
+    else if(Average>=Grade_C)return 'C';
+    else if(Average>=Grade_D)return 'D';
     else return 'F';
 }
 
@@ -137,4 +138,13 @@ void printPerformance(float Average, int Performance)
         printf("\n");
     }
     printf("\n");
+}
+
+//Function to check if marks are valid.
+bool checkMarks(int Marks1,int Marks2,int Marks3)
+{
+    if(Marks1 < 0 || Marks1 > MAX_MARKS ||
+    Marks2 < 0 || Marks2 > MAX_MARKS ||
+    Marks3 < 0 || Marks3 > MAX_MARKS)return true;
+    else return false;
 }
