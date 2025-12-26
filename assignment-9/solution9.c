6169b0283a051a78b99982fa62d7b787765ea363
Sai Sandeep R <sandeep.5112004@gmail.com>
2025-12-26 10:45:31 +0530

    Updated FCFS bsed OS Schedule.

--- assignment-9/solution9.c
+++ assignment-9/solution9.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 50
+#define MAX_PROC 50
 #define MAX_PID 1000
 
 #define READY 0
@@ -12,49 +12,174 @@
 #define KILLED 4
 
 typedef struct {
-    char name[40];
+    char pname[40];
     int pid;
-    int cpuBurst, ioStart, ioDuration;
-    int executedCpu, remainingCpu, ioLeft;
-    int finishTime, killedTime;
+    int cpuTime;
+    int ioAt;
+    int ioTime;
+    int cpuDone;
+    int cpuLeft;
+    int ioLeft;
+    int endTime;
+    int killedAt;
     int state;
 } Process;
 
 typedef struct {
-    int q[200];
-    int f, r;
+    int data[200];
+    int front;
+    int rear;
 } Queue;
 
-void initQ(Queue *q) { q->f = q->r = 0; }
-int empty(Queue *q) { return q->f == q->r; }
-void push(Queue *q, int x) { q->q[q->r++] = x; }
-int pop(Queue *q) { return empty(q) ? -1 : q->q[q->f++]; }
+void initQueue(Queue *q) {
+    q->front = q->rear = 0;
+}
+
+int isEmpty(Queue *q) {
+    return q->front == q->rear;
+}
+
+void enqueue(Queue *q, int val) {
+    q->data[q->rear++] = val;
+}
+
+int dequeue(Queue *q) {
+    if (isEmpty(q)) return -1;
+    return q->data[q->front++];
+}
+
+void loadProcesses(Process proc[], int count, int pidIndex[]) {
+    for (int i = 0; i < count; i++) {
+        char x[10], y[10];
+        scanf("%s %d %d %s %s",
+              proc[i].pname,
+              &proc[i].pid,
+              &proc[i].cpuTime,
+              x, y);
+
+        proc[i].ioAt = strcmp(x, "-") == 0 ? -1 : atoi(x);
+        proc[i].ioTime = strcmp(y, "-") == 0 ? 0 : atoi(y);
+
+        proc[i].cpuDone = 0;
+        proc[i].cpuLeft = proc[i].cpuTime;
+        proc[i].ioLeft = 0;
+        proc[i].endTime = -1;
+        proc[i].killedAt = -1;
+        proc[i].state = READY;
+
+        pidIndex[proc[i].pid] = i;
+    }
+}
+
+void sortKillEvents(int pid[], int time[], int k) {
+    for (int i = 0; i < k; i++) {
+        for (int j = i + 1; j < k; j++) {
+            if (time[j] < time[i]) {
+                int t = time[i]; time[i] = time[j]; time[j] = t;
+                int p = pid[i]; pid[i] = pid[j]; pid[j] = p;
+            }
+        }
+    }
+}
+
+void applyKillEvents(Process proc[], int pidIndex[], int currentTime,
+                     int killPid[], int killTime[],
+                     int *killPtr, int totalKills,
+                     int *runningIdx, int *doneCount) {
+
+    while (*killPtr < totalKills && killTime[*killPtr] == currentTime) {
+        int idx = pidIndex[killPid[*killPtr]];
+        if (idx != -1 && proc[idx].state < TERMINATED) {
+            proc[idx].state = KILLED;
+            proc[idx].killedAt = currentTime;
+            proc[idx].endTime = currentTime;
+            (*doneCount)++;
+            if (*runningIdx == idx)
+                *runningIdx = -1;
+        }
+        (*killPtr)++;
+    }
+}
+
+void runCPU(Process proc[], Queue *readyQ, Queue *waitQ,
+            int *runningIdx, int currentTime, int *doneCount) {
+
+    if (*runningIdx == -1 && !isEmpty(readyQ)) {
+        *runningIdx = dequeue(readyQ);
+        proc[*runningIdx].state = RUNNING;
+    }
+
+    if (*runningIdx != -1) {
+        proc[*runningIdx].cpuDone++;
+        proc[*runningIdx].cpuLeft--;
+
+        if (proc[*runningIdx].cpuLeft == 0) {
+            proc[*runningIdx].state = TERMINATED;
+            proc[*runningIdx].endTime = currentTime + 1;
+            (*doneCount)++;
+            *runningIdx = -1;
+        }
+        else if (proc[*runningIdx].ioAt >= 0 &&
+                 proc[*runningIdx].cpuDone == proc[*runningIdx].ioAt &&
+                 proc[*runningIdx].ioTime > 0) {
+
+            proc[*runningIdx].state = WAITING;
+            proc[*runningIdx].ioLeft = proc[*runningIdx].ioTime;
+            enqueue(waitQ, *runningIdx);
+            *runningIdx = -1;
+        }
+    }
+}
+
+void updateIO(Process proc[], Queue *readyQ, Queue *waitQ) {
+    Queue tempQ;
+    initQueue(&tempQ);
+
+    while (!isEmpty(waitQ)) {
+        int idx = dequeue(waitQ);
+        if (proc[idx].state == KILLED) continue;
+
+        proc[idx].ioLeft--;
+        if (proc[idx].ioLeft == 0) {
+            proc[idx].state = READY;
+            enqueue(readyQ, idx);
+        } else {
+            enqueue(&tempQ, idx);
+        }
+    }
+    *waitQ = tempQ;
+}
+
+void printOutput(Process proc[], int count) {
+    for (int i = 0; i < count; i++) {
+        if (proc[i].state == KILLED) {
+            printf("%d %s %d %d KILLED at %d - -\n",
+                   proc[i].pid,
+                   proc[i].pname,
+                   proc[i].cpuTime,
+                   proc[i].ioTime,
+                   proc[i].killedAt);
+        } else {
+            int tat = proc[i].endTime;
+            int wt = tat - proc[i].cpuTime - proc[i].ioTime;
+            printf("%d %s %d %d %d %d\n",
+                   proc[i].pid,
+                   proc[i].pname,
+                   proc[i].cpuTime,
+                   proc[i].ioTime,
+                   tat, wt);
+        }
+    }
+}
 
 int main() {
-    Process p[MAX];
-    int pidMap[MAX_PID];
-    memset(pidMap, -1, sizeof(pidMap));
+    Process proc[MAX_PROC];
+    int pidIndex[MAX_PID];
+    memset(pidIndex, -1, sizeof(pidIndex));
 
     int n;
     scanf("%d", &n);
-
-    for (int i = 0; i < n; i++) {
-        char a[10], b[10];
-        scanf("%s %d %d %s %s",
-              p[i].name, &p[i].pid, &p[i].cpuBurst, a, b);
-
-        p[i].ioStart = strcmp(a, "-") == 0 ? -1 : atoi(a);
-        p[i].ioDuration = strcmp(b, "-") == 0 ? 0 : atoi(b);
-
-        p[i].executedCpu = 0;
-        p[i].remainingCpu = p[i].cpuBurst;
-        p[i].ioLeft = 0;
-        p[i].finishTime = -1;
-        p[i].killedTime = -1;
-        p[i].state = READY;
-
-        pidMap[p[i].pid] = i;
-    }
+    loadProcesses(proc, n, pidIndex);
 
     int k;
     scanf("%d", &k);
@@ -63,95 +188,34 @@ int main() {
     for (int i = 0; i < k; i++)
         scanf("%d %d", &killPid[i], &killTime[i]);
 
-    for (int i = 0; i < k; i++)
-        for (int j = i + 1; j < k; j++)
-            if (killTime[j] < killTime[i]) {
-                int t = killTime[i]; killTime[i] = killTime[j]; killTime[j] = t;
-                int x = killPid[i]; killPid[i] = killPid[j]; killPid[j] = x;
-            }
+    sortKillEvents(killPid, killTime, k);
 
-    Queue ready, waiting;
-    initQ(&ready);
-    initQ(&waiting);
+    Queue readyQ, waitQ;
+    initQueue(&readyQ);
+    initQueue(&waitQ);
 
     for (int i = 0; i < n; i++)
-        push(&ready, i);
+        enqueue(&readyQ, i);
 
-    int time = 0, finished = 0;
-    int running = -1, killIdx = 0;
+    int currentTime = 0;
+    int doneCount = 0;
+    int runningIdx = -1;
+    int killPtr = 0;
 
-    while (finished < n) {
+    while (doneCount < n) {
+        applyKillEvents(proc, pidIndex, currentTime,
+                        killPid, killTime,
+                        &killPtr, k,
+                        &runningIdx, &doneCount);
 
-        while (killIdx < k && killTime[killIdx] == time) {
-            int idx = pidMap[killPid[killIdx]];
-            if (idx != -1 && p[idx].state < TERMINATED) {
-                p[idx].state = KILLED;
-                p[idx].killedTime = time;
-                p[idx].finishTime = time;
-                finished++;
-                if (running == idx) running = -1;
-            }
-            killIdx++;
-        }
+        runCPU(proc, &readyQ, &waitQ,
+               &runningIdx, currentTime, &doneCount);
 
-        if (running == -1 && !empty(&ready)) {
-            running = pop(&ready);
-            p[running].state = RUNNING;
-        }
+        updateIO(proc, &readyQ, &waitQ);
 
-        if (running != -1) {
-            p[running].executedCpu++;
-            p[running].remainingCpu--;
-
-            if (p[running].remainingCpu == 0) {
-                p[running].state = TERMINATED;
-                p[running].finishTime = time + 1;
-                finished++;
-                running = -1;
-            }
-            else if (p[running].ioStart >= 0 &&
-                     p[running].executedCpu == p[running].ioStart &&
-                     p[running].ioDuration > 0) {
-                p[running].state = WAITING;
-                p[running].ioLeft = p[running].ioDuration;
-                push(&waiting, running);
-                running = -1;
-            }
-        }
-
-        Queue nextWait;
-        initQ(&nextWait);
-        while (!empty(&waiting)) {
-            int idx = pop(&waiting);
-            if (p[idx].state == KILLED) continue;
-            p[idx].ioLeft--;
-            if (p[idx].ioLeft == 0) {
-                p[idx].state = READY;
-                push(&ready, idx);
-            } else {
-                push(&nextWait, idx);
-            }
-        }
-        waiting = nextWait;
-
-        time++;
+        currentTime++;
     }
 
-    for (int i = 0; i < n; i++) {
-        if (p[i].state == KILLED) {
-            printf("%d %s %d %d KILLED at %d - -\n",
-                   p[i].pid, p[i].name,
-                   p[i].cpuBurst, p[i].ioDuration,
-                   p[i].killedTime);
-        } else {
-            int tat = p[i].finishTime;
-            int wt = tat - p[i].cpuBurst - p[i].ioDuration;
-            printf("%d %s %d %d %d %d\n",
-                   p[i].pid, p[i].name,
-                   p[i].cpuBurst, p[i].ioDuration,
-                   tat, wt);
-        }
-    }
-
+    printOutput(proc, n);
     return 0;
 }
